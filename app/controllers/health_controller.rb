cf8e88e1823d4d33af9da115c1923492e8e00048
Luis Arturo Mendoza <lmendoza@morninc.com>
2021-02-01 09:17:02 -0600

    fix: Add welcome route to /

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,5 +1,8 @@
 class HealthController < ApplicationController
   def health
     render json: {api: 'OK'}, status: :ok
+    end
+  def welcome
+    render json: {welcome: 'Coding Exercise', author: 'LuisArturoMR', start: 'Read README.md'}, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/health', to: 'health#health'
+  get '/', to: 'health#welcome'
 
   resources :rewards, only: [:index]
   resources :user_events, only: [:create]
